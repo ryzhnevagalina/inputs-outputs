32d05212689bc5cb33a3d4b141dc89093d7ec14f
ryzhnevagalina <66208921+ryzhnevagalina@users.noreply.github.com>
2020-06-13 22:59:21 +0300

    Add files via upload

---
+++ Contest_1(test) (1).py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# \begin{problem}{Любит/не любит}{stdin}{stdout}{1 second(s)}{64 MiB}
+# Девушка гадает по ромашке. Она пытается выбрать - любят её или нет. Определите долю ответов "любит" среди всех лепестков.
+# \InputFile
+# 
+# \OutputFile
+# 
+# \Examples
+# \begin{example}
+# \exmp{loves not loves loves}{0.75}
+# \end{example}\Note
+#  При сдаче этого задания выберите компилятор make2
+# \end{problem}
